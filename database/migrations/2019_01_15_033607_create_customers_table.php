4cca5024ee61286dff7c3aba05f4ce4f93889d3e
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-01-15 10:51:27 +0700

    Create database migration admins,products,customers,bills and setup IndexLenth

---
+++ database/migrations/2019_01_15_033607_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',100);
+            $table->string('email')->unique();
+            $table->text('address');
+            $table->enum('status',['ACTIVE','NOT_ACTIVE']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ database/migrations/2019_01_15_033712_create_bills_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('transaction_date');
+            $table->integer('customer_id')->unsigned();
+            $table->integer('product_id')->unsigned();
+            $table->integer('total');
+            $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
